7ad826f98c6a81bb99bef0c271ad83e53cb2145d
Giovanni Montanari <giovannimontanari3@gmail.com>
2024-10-24 09:45:20 -0300

    column and row

--- app/src/main/java/com/example/studycompose/MainActivity.kt
+++ app/src/main/java/com/example/studycompose/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.studycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +23,18 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun SetupView() {
-        CustomText(text = "Primeira função composable")
+        Column(Modifier.background(color = Color.Gray)) {
+            CustomText(text = "Primeira função composable")
+            Row {
+                CustomText(text = "Seja bem vindo!!")
+                CustomText(text = "Olá, usuário")
+            }
+        }
     }
 
     @Composable
     private fun CustomText(text: String) {
-        Text(text = text)
+        Text(text = text, color = Color.White)
     }
 
     @Preview
