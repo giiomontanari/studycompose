2865c18083df7c462aa4f48ac1f5d1d568a3684c
Giovanni Montanari <giovannimontanari3@gmail.com>
2024-10-24 09:53:32 -0300

    add box into view

--- app/src/main/java/com/example/studycompose/MainActivity.kt
+++ app/src/main/java/com/example/studycompose/MainActivity.kt
@@ -4,20 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SetupView()
+//            SetupView()
+            NotificationBage()
         }
     }
 
@@ -37,9 +45,28 @@ class MainActivity : ComponentActivity() {
         Text(text = text, color = Color.White)
     }
 
+    @Composable
+    private fun NotificationBage() {
+        Box(
+            modifier = Modifier.padding(20.dp), contentAlignment = Alignment.Center
+        ) {
+            Box(modifier = Modifier
+                .size(24.dp)
+                .clip(CircleShape)
+                .background(color = Color.Blue))
+            Box(modifier = Modifier
+                .size(8.dp)
+                .clip(CircleShape)
+                .background(color = Color.Red)
+                .align(Alignment.TopEnd)
+            )
+        }
+    }
+
     @Preview
     @Composable
     fun Preview() {
         SetupView()
+        NotificationBage()
     }
 }
