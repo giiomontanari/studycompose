2ee933fb00cb3632eb048736aab625d42acd8cee
Giovanni Montanari <giovannimontanari3@gmail.com>
2024-10-24 09:37:19 -0300

    add preview function

--- app/src/main/java/com/example/studycompose/MainActivity.kt
+++ app/src/main/java/com/example/studycompose/MainActivity.kt
@@ -5,18 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           CustomText(text = getString(R.string.hello_world))
+            SetupView()
         }
+    }
+
+    @Composable
+    private fun SetupView() {
+        CustomText(text = "Primeira função composable")
     }
 
     @Composable
     private fun CustomText(text: String) {
         Text(text = text)
+    }
+
+    @Preview
+    @Composable
+    fun Preview() {
+        SetupView()
     }
 }
